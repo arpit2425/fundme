d7ff6c65afc97d8a9e09d00b0a6f1555232d8b04
arpit <arpittrivedi2425@gmail.com>
2026-04-01 08:37:52 +0530

    fix all test cases

--- anchor/tests/fundus.spec.ts
+++ anchor/tests/fundus.spec.ts
@@ -101,230 +101,235 @@ describe('fundus', () => {
     console.log('Campaign After Update:', campaignAfter)
   })
 
-  it('deletes a campaign', async () => {
-    const creator = provider.wallet
-    // Derive Campaign PDA
+  // it('deletes a campaign', async () => {
+  //   const creator = provider.wallet
+  //   // Derive Campaign PDA
+  //   const [campaignPda] = await PublicKey.findProgramAddress(
+  //     [Buffer.from('campaign'), CID.toBuffer('le', 8)],
+  //     program.programId
+  //   )
+
+  //   // Fetch campaign before deletion
+  //   const campaignBefore = await program.account.campaign.fetch(campaignPda)
+  //   console.log('Campaign Before Deletion:', campaignBefore)
+
+  //   // Execute the deletion
+  //   const tx = await program.rpc.deleteCampaign(CID, {
+  //     accounts: {
+  //       creater:creator.publicKey,
+  //       campaign: campaignPda,
+  //       systemProgram: SystemProgram.programId,
+  //     },
+  //   })
+
+  //   console.log('Transaction Signature:', tx)
+
+  //   const campaignAfter = await program.account.campaign.fetch(campaignPda)
+  //   console.log('Campaign After Deletion:', campaignAfter)
+  // })
+
+  it('donate to campaign', async () => {
+    const donor = provider.wallet
+
     const [campaignPda] = await PublicKey.findProgramAddress(
-      [Buffer.from('campaign'), CID.toBuffer('le', 8)],
+      [Buffer.from('campaign'), CID.toArrayLike(Buffer, 'le', 8)],
       program.programId
     )
 
-    // Fetch campaign before deletion
-    const campaignBefore = await program.account.campaign.fetch(campaignPda)
-    console.log('Campaign Before Deletion:', campaignBefore)
 
-    // Execute the deletion
-    const tx = await program.rpc.deleteCampaign(CID, {
+    const [contributionPda] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from('donar'),
+        donor.publicKey.toBuffer(),
+        CID.toArrayLike(Buffer, 'le', 8),
+        DONOR_COUNT.add(new anchor.BN(1)).toArrayLike(Buffer, 'le', 8),
+      ],
+      program.programId
+    )
+
+    const donorBefore = await provider.connection.getBalance(donor.publicKey)
+    const campaignBefore = await provider.connection.getBalance(campaignPda)
+    console.log("donar amount before :",donorBefore)
+    console.log("campaign before :", campaignBefore)
+    console.log("cid:",CID)
+
+    const donation_amount = new anchor.BN(Math.round(4 * LAMPORTS_PER_SOL)) // 1.5 SOL in lamports
+    const tx = await program.rpc.donate(CID, donation_amount, {
       accounts: {
-        creater:creator.publicKey,
+       donar:donor.publicKey,
         campaign: campaignPda,
+        transaction: contributionPda,
         systemProgram: SystemProgram.programId,
       },
     })
 
     console.log('Transaction Signature:', tx)
 
-    const campaignAfter = await program.account.campaign.fetch(campaignPda)
-    console.log('Campaign After Deletion:', campaignAfter)
+    const donorAfter = await provider.connection.getBalance(donor.publicKey)
+    const campaignAfter = await provider.connection.getBalance(campaignPda)
+
+    const contribution = await program.account.transaction.fetch(
+      contributionPda
+    )
+    console.log('Contribution:', contribution)
+
+    console.log(
+      `
+        donorBefore: ${donorBefore},
+        donorAfter: ${donorAfter},
+        donation_amount: ${donation_amount.toNumber()}
+      `
+    )
+    console.log(
+      `
+        campaignBefore: ${campaignBefore},
+        campaignAfter: ${campaignAfter},
+        donation_amount: ${donation_amount.toNumber()}
+      `
+    )
   })
 
-  // it('donate to campaign', async () => {
-  //   const donor = provider.wallet
+  it('withdraws from campaign', async () => {
+    const withdrawer = provider.wallet
 
-  //   const [campaignPda] = await PublicKey.findProgramAddress(
-  //     [Buffer.from('campaign'), CID.toArrayLike(Buffer, 'le', 8)],
-  //     program.programId
-  //   )
+    // Derive PDAs
+    const [programStatePda] = await PublicKey.findProgramAddress(
+      [Buffer.from('program_state')],
+      program.programId
+    )
 
-  //   const [contributionPda] = await PublicKey.findProgramAddress(
-  //     [
-  //       Buffer.from('donor'),
-  //       donor.publicKey.toBuffer(),
-  //       CID.toArrayLike(Buffer, 'le', 8),
-  //       DONOR_COUNT.add(new anchor.BN(1)).toArrayLike(Buffer, 'le', 8),
-  //     ],
-  //     program.programId
-  //   )
+    const [campaignPda] = await PublicKey.findProgramAddress(
+      [Buffer.from('campaign'), CID.toArrayLike(Buffer, 'le', 8)],
+      program.programId
+    )
 
-  //   const donorBefore = await provider.connection.getBalance(donor.publicKey)
-  //   const campaignBefore = await provider.connection.getBalance(campaignPda)
+    const [withdrawalPda] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from('withdraws'),
+        withdrawer.publicKey.toBuffer(),
+        CID.toArrayLike(Buffer, 'le', 8),
+        WITHDRAWAL_COUNT.add(new anchor.BN(1)).toArrayLike(Buffer, 'le', 8),
+      ],
+      program.programId
+    )
 
-  //   const donation_amount = new anchor.BN(Math.round(10.5 * 1_000_000_000)) // 1.5 SOL in lamports
-  //   const tx = await program.rpc.donate(CID, donation_amount, {
-  //     accounts: {
-  //      donar:donor.publicKey,
-  //       campaign: campaignPda,
-  //       transaction: contributionPda,
-  //       systemProgram: SystemProgram.programId,
-  //     },
-  //   })
+    // Fetch current balances
+    const withdrawerBalanceBefore = await provider.connection.getBalance(
+      withdrawer.publicKey
+    )
+    const campaignBalanceBefore = await provider.connection.getBalance(
+      campaignPda
+    )
 
-  //   console.log('Transaction Signature:', tx)
+    // Fetch program state for platform info
+    const programState = await program.account.programState.fetch(
+      programStatePda
+    )
+    const platformAddress = programState.platformAddress
+    const platformBalanceBefore = await provider.connection.getBalance(
+      platformAddress
+    )
 
-  //   const donorAfter = await provider.connection.getBalance(donor.publicKey)
-  //   const campaignAfter = await provider.connection.getBalance(campaignPda)
+    // Define withdrawal amount (1.5 SOL in lamports)
+    const withdrawalAmount = new anchor.BN(Math.round(1.5 * 1_000_000_000))
 
-  //   const contribution = await program.account.transaction.fetch(
-  //     contributionPda
-  //   )
-  //   console.log('Contribution:', contribution)
+    // Execute the withdrawal
+    const tx = await program.rpc.withdraw(CID, withdrawalAmount, {
+      accounts: {
+       donar:withdrawer.publicKey,
+        campaign: campaignPda,
+        transaction: withdrawalPda,
+        platformAddress: platformAddress,
+        programState: programStatePda,
+        systemProgram: SystemProgram.programId,
+      },
+    })
+    console.log('Transaction Signature:', tx)
 
-  //   console.log(
-  //     `
-  //       donorBefore: ${donorBefore},
-  //       donorAfter: ${donorAfter},
-  //       donation_amount: ${donation_amount.toNumber()}
-  //     `
-  //   )
-  //   console.log(
-  //     `
-  //       campaignBefore: ${campaignBefore},
-  //       campaignAfter: ${campaignAfter},
-  //       donation_amount: ${donation_amount.toNumber()}
-  //     `
-  //   )
-  // })
+    // Fetch updated balances
+    const withdrawerBalanceAfter = await provider.connection.getBalance(
+      withdrawer.publicKey
+    )
+    const campaignBalanceAfter = await provider.connection.getBalance(
+      campaignPda
+    )
+    const platformBalanceAfter = await provider.connection.getBalance(
+      platformAddress
+    )
 
-  // it('withdraws from campaign', async () => {
-  //   const withdrawer = provider.wallet
+    // Fetch withdrawal account details
+    const withdrawal = await program.account.transaction.fetch(withdrawalPda)
+    console.log('Withdrawal Details:', withdrawal)
 
-  //   // Derive PDAs
-  //   const [programStatePda] = await PublicKey.findProgramAddress(
-  //     [Buffer.from('program_state')],
-  //     program.programId
-  //   )
+    // Calculate expected platform fee
+    const platformFeePercentage = programState.platformFee.toNumber()
+    const platformFee = Math.floor(
+      (withdrawalAmount.toNumber() * platformFeePercentage) / 100
+    )
+    const creatorAmount = withdrawalAmount.toNumber() - platformFee
 
-  //   const [campaignPda] = await PublicKey.findProgramAddress(
-  //     [Buffer.from('campaign'), CID.toArrayLike(Buffer, 'le', 8)],
-  //     program.programId
-  //   )
+    // Log balances
+    console.log(`
+      Withdrawer Balance:
+        Before: ${withdrawerBalanceBefore} lamports
+        After:  ${withdrawerBalanceAfter} lamports
+    `)
+    console.log(`
+      Campaign Balance:
+        Before: ${campaignBalanceBefore} lamports
+        After:  ${campaignBalanceAfter} lamports
+    `)
+    console.log(`
+      Platform Balance:
+        Before: ${platformBalanceBefore} lamports
+        After:  ${platformBalanceAfter} lamports
+        Fee Deducted: ${platformFee} lamports
+    `)
+    console.log(`
+      Withdrawal Amount:
+        Total: ${withdrawalAmount.toNumber()} lamports
+        Creator's Share: ${creatorAmount} lamports
+        Platform's Share: ${platformFee} lamports
+    `)
+  })
 
-  //   const [withdrawalPda] = await PublicKey.findProgramAddress(
-  //     [
-  //       Buffer.from('withdraw'),
-  //       withdrawer.publicKey.toBuffer(),
-  //       CID.toArrayLike(Buffer, 'le', 8),
-  //       WITHDRAWAL_COUNT.add(new anchor.BN(1)).toArrayLike(Buffer, 'le', 8),
-  //     ],
-  //     program.programId
-  //   )
+  it('updates platform settings', async () => {
+    const updater = provider.wallet
 
-  //   // Fetch current balances
-  //   const withdrawerBalanceBefore = await provider.connection.getBalance(
-  //     withdrawer.publicKey
-  //   )
-  //   const campaignBalanceBefore = await provider.connection.getBalance(
-  //     campaignPda
-  //   )
+    // Derive the Program State PDA
+    const [programStatePda] = await PublicKey.findProgramAddress(
+      [Buffer.from('program_state')],
+      program.programId
+    )
 
-  //   // Fetch program state for platform info
-  //   const programState = await program.account.programState.fetch(
-  //     programStatePda
-  //   )
-  //   const platformAddress = programState.platformAddress
-  //   const platformBalanceBefore = await provider.connection.getBalance(
-  //     platformAddress
-  //   )
+    // Fetch current state
+    const programStateBefore = await program.account.programState.fetch(
+      programStatePda
+    )
+    console.log(
+      'Program State Before:',
+      (programStateBefore.platformFee).toString()
+    )
 
-  //   // Define withdrawal amount (1.5 SOL in lamports)
-  //   const withdrawalAmount = new anchor.BN(Math.round(1.5 * 1_000_000_000))
+    // Generate new platform address and fee
+    const newPlatformFee = 10 // New platform fee in percentage
 
-  //   // Execute the withdrawal
-  //   const tx = await program.rpc.withdraw(CID, withdrawalAmount, {
-  //     accounts: {
-  //      donar:withdrawer.publicKey,
-  //       campaign: campaignPda,
-  //       transaction: withdrawalPda,
-  //       platformAddress: platformAddress,
-  //       programState: programStatePda,
-  //       systemProgram: SystemProgram.programId,
-  //     },
-  //   })
-  //   console.log('Transaction Signature:', tx)
+    // Update platform settings
+    await program.rpc.updatePlatformSettings(new anchor.BN(newPlatformFee), {
+      accounts: {
+        updater: updater.publicKey,
+        programState: programStatePda,
+        systemProgram:SystemProgram.programId,
+      },
+    })
 
-  //   // Fetch updated balances
-  //   const withdrawerBalanceAfter = await provider.connection.getBalance(
-  //     withdrawer.publicKey
-  //   )
-  //   const campaignBalanceAfter = await provider.connection.getBalance(
-  //     campaignPda
-  //   )
-  //   const platformBalanceAfter = await provider.connection.getBalance(
-  //     platformAddress
-  //   )
-
-  //   // Fetch withdrawal account details
-  //   const withdrawal = await program.account.transaction.fetch(withdrawalPda)
-  //   console.log('Withdrawal Details:', withdrawal)
-
-  //   // Calculate expected platform fee
-  //   const platformFeePercentage = programState.platformFee.toNumber()
-  //   const platformFee = Math.floor(
-  //     (withdrawalAmount.toNumber() * platformFeePercentage) / 100
-  //   )
-  //   const creatorAmount = withdrawalAmount.toNumber() - platformFee
-
-  //   // Log balances
-  //   console.log(`
-  //     Withdrawer Balance:
-  //       Before: ${withdrawerBalanceBefore} lamports
-  //       After:  ${withdrawerBalanceAfter} lamports
-  //   `)
-  //   console.log(`
-  //     Campaign Balance:
-  //       Before: ${campaignBalanceBefore} lamports
-  //       After:  ${campaignBalanceAfter} lamports
-  //   `)
-  //   console.log(`
-  //     Platform Balance:
-  //       Before: ${platformBalanceBefore} lamports
-  //       After:  ${platformBalanceAfter} lamports
-  //       Fee Deducted: ${platformFee} lamports
-  //   `)
-  //   console.log(`
-  //     Withdrawal Amount:
-  //       Total: ${withdrawalAmount.toNumber()} lamports
-  //       Creator's Share: ${creatorAmount} lamports
-  //       Platform's Share: ${platformFee} lamports
-  //   `)
-  // })
-
-  // it('updates platform settings', async () => {
-  //   const updater = provider.wallet
-
-  //   // Derive the Program State PDA
-  //   const [programStatePda] = await PublicKey.findProgramAddress(
-  //     [Buffer.from('program_state')],
-  //     program.programId
-  //   )
-
-  //   // Fetch current state
-  //   const programStateBefore = await program.account.programState.fetch(
-  //     programStatePda
-  //   )
-  //   console.log(
-  //     'Program State Before:',
-  //     (programStateBefore.platformFee as string).toString()
-  //   )
-
-  //   // Generate new platform address and fee
-  //   const newPlatformFee = 10 // New platform fee in percentage
-
-  //   // Update platform settings
-  //   await program.rpc.updatePlatformSettings(new anchor.BN(newPlatformFee), {
-  //     accounts: {
-  //       updater: updater.publicKey,
-  //       programState: programStatePda,
-  //     },
-  //   })
-
-  //   // Fetch updated state
-  //   const programStateAfter = await program.account.programState.fetch(
-  //     programStatePda
-  //   )
-  //   console.log(
-  //     'Program State After:',
-  //     (programStateAfter.platformFee as string).toString()
-  //   )
-  // })
+    // Fetch updated state
+    const programStateAfter = await program.account.programState.fetch(
+      programStatePda
+    )
+    console.log(
+      'Program State After:',
+      (programStateAfter.platformFee ).toString()
+    )
+  })
 })
